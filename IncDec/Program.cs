f89b2326e0cb0ad1f8a6ae733ae8390d8fc66c20
andrecunha6 <andrefncunha@gmail.com>
2019-03-15 16:14:16 +0000

    aula3

---
+++ IncDec/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = -5;
+            int c;
+
+            c = a++ + b++;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
