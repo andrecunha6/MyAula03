34766ccadeda4da9ee60c0a00eeb8b4c3a3963d7
andrecunha6 <andrefncunha@gmail.com>
2019-03-15 17:03:18 +0000

    Piramide

---
+++ Piramide/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+//Exercicio 3
+//Declarar variaveis
+//pedir numero de linhas ao user
+//mostrar piramide
+
+namespace Piramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nLinhasString;
+            int nLinhas;
+            Console.WriteLine("inserir numero de linhas");
+            nLinhasString = Console.ReadLine();
+            nLinhas = Convert.ToInt32(nLinhasString);
+            for (int i = 0; i < nLinhas; ++i)
+            {
+                int numEspaco, numAst;
+                numEspaco = nLinhas-i-1;
+                numAst = i*2+1;
+                for (int j = 0; j<numEspaco; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j=0; j<numAst; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
