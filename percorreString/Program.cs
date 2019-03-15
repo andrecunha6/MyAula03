bda316aca20092df292c268ea9b50cf41806cb35
andrecunha6 <andrefncunha@gmail.com>
2019-03-15 17:39:50 +0000

    percorreString

---
+++ percorreString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace percorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, cString;
+            char c;
+            Console.WriteLine("Digite uma string.");
+            s = Console.ReadLine();
+            Console.WriteLine("Digite um char.");
+            cString = Console.ReadLine();
+            c = cString[0];
+            foreach (char cAtual in s)
+            {
+               if (cAtual != c)
+               {
+                    Console.Write(cAtual);
+               }
+            }
+            Console.WriteLine();
+        }
+    }
+}
