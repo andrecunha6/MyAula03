33f2cef12cac3d11aade9f1b1717df9e2194b295
andrecunha6 <andrefncunha@gmail.com>
2019-03-15 17:59:34 +0000

    matrizes

---
+++ ArrayBiDim/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ArrayBiDim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("")
+        }
+    }
+}
+//variaveis, pedir nlinhas/ncolunas, pedir valores para matriz, imprimir matriz, calcular medias das linhas, somar medias, imprimir medias, imprimir somas.
